7bc3d3afa6292b3c0512730d6700d17251092d11
raymond zhao <raymondzhao158@gmail.com>
2021-04-28 11:34:49 -0700

    commit tree 2fbb959cdeb52d250d32a7163858e7d2e69622bc

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import { render } from "enzyme"
+import React, { Component } from "react"
+export default class bomb extends Component{
+    constructor(props){
+        super()
+        this.state = {secondsLeft:props.initialCount}
+
+    }
+//     render(){
+//     if(this.state.SecondsLeft === 0 ){
+//         return(<div>
+//             {this.state.secondsLeft} seconds left before I go boom!
+//         </div>
+//         )}
+//         else{
+//             return(
+//                 <div>Boom!</div>
+//             )
+//         }
+
+    
+// }
+render() {
+    return this.state.secondsLeft === 0 ? `Boom!` : `${this.state.secondsLeft} seconds left before I go boom!`
+ }
+}
+
+
